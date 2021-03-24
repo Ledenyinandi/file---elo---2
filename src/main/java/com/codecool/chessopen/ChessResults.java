d16332016e4dcea07ac60a65655f52ef9ff21bd2
Ledenyinandi <ledenyinandi@gmail.com>
2021-03-24 16:17:30 +0100

    Tests are working

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,41 @@
 package com.codecool.chessopen;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        return new ArrayList<>(sortMap(readFile(fileName)).keySet());
     }
 
+    public Map<String, Integer> readFile(String fileName) {
+        Map<String, Integer> results = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String[] inputFromFile;
+            int points;
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputFromFile = line.split(",");
+                points = Integer.parseInt(inputFromFile[1]) + Integer.parseInt(inputFromFile[2]) +
+                        Integer.parseInt(inputFromFile[3]) + Integer.parseInt(inputFromFile[4]) +
+                        Integer.parseInt(inputFromFile[5]);
+                results.put(inputFromFile[0], points);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+        return results;
+    }
+
+    public Map<String, Integer> sortMap(Map<String, Integer> mapToSort) {
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        mapToSort.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+        return sortedMap;
+    }
 }
